3132eac278b8fc1270a830e083269b3183cecbf1
梁栋 <120848369@qq.com>
2017-04-21 10:04:10 +0800

    touch event bugfix

--- carousel.js
+++ carousel.js
@@ -4,12 +4,16 @@
  * @param option
  */
 $.installCarousel = function(container, option) {
-    // 当前的触摸事件
-    var touchEvent = null;
+    // 触摸容器的手指集合
+    var touchFingers = {};
+    // 集合大小
+    var fingerCount = 0;
+
+    // 触摸事件的唯一ID
+    var touchEventID = 0;
     // 当前的翻页事件
-    var slideEvent = null;
-    // 正在触屏的手指数量
-    var touchFinger = 0;
+    var slideEventID = 0;
+
     // 当前图片下标
     var curIndex = 0;
     // 当前X轴偏移
@@ -114,7 +118,7 @@ $.installCarousel = function(container, option) {
                 } else if (index == renderImages.length - 1) { // 右边界
                     setImage(1); // 偷梁换柱为第二张图片
                 }
-                slideEvent = null;
+                slideEventID = 0;
             }
         });
 
@@ -135,17 +139,17 @@ $.installCarousel = function(container, option) {
         for (var i = 0; i < event.originalEvent.targetTouches.length; ++i) {
             var touch = event.originalEvent.targetTouches[i];
             identSet[touch.identifier] = true;
-            if (touchEvent[touch.identifier] === undefined) {
-                touchEvent[touch.identifier] = null;
-                ++touchFinger;
+            if (touchFingers[touch.identifier] === undefined) {
+                touchFingers[touch.identifier] = null;
+                ++fingerCount;
             }
         }
         // 将已删除的手指集合清理
-        for (var identifier in touchEvent) {
+        for (var identifier in touchFingers) {
             // 浏览器集合中已不存在,删除
             if (identSet[identifier] === undefined) {
-                delete(touchEvent[identifier]);
-                --touchFinger;
+                delete(touchFingers[identifier]);
+                --fingerCount;
             }
         }
     }
@@ -154,101 +158,84 @@ $.installCarousel = function(container, option) {
     if (renderImages.length > 1) {
         // 右边界
         var rightBound = (renderImages.length - 1) * width * -1;
-        // 父容器注册下拉事件
-        $(container).on("touchstart", function (event) {
-            // 防止事件向body冒泡
-            event.stopPropagation();
 
-            if (!touchEvent) {
-                // 第一根指头触屏, 产生新的触摸事件
-                touchEvent = {};
-            }
-            // 将本次的手指加入到触摸事件集合中
+        // 统一处理
+        $(container).on("touchstart touchmove touchend touchcancel", function(event) {
+            var beforeFingerCount = fingerCount;
             compareTouchFingers(event);
-            // 翻页事件正在进行,忽略本次触屏
-            if (slideEvent) {
-                return;
-            }
-            // 产生新的翻页事件
-            slideEvent = touchEvent;
-            // 触摸开始时间
-            touchStartTime = new Date().getTime();
-            // 暂停动画
-            imageList.removeClass("slideTrans");
-            imageList.unbind();
-        }).on("touchmove", function (event) {
-            // 防止事件向body冒泡
-            event.stopPropagation();
 
-            // 在翻页未完成前触摸,将被忽略
-            if (touchEvent != slideEvent) {
-                return;
-            }
-            // 计算每个变化的手指, 取变化最大的delta
-            var maxDelta = 0;
-            for (var i = 0; i < event.originalEvent.changedTouches.length; ++i) {
-                var fingerTouch = event.originalEvent.changedTouches[i];
-                if (touchEvent[fingerTouch.identifier] !== undefined) {
-                    if (touchEvent[fingerTouch.identifier] !== null) {
-                        var delta = fingerTouch.clientX - touchEvent[fingerTouch.identifier];
-                        if (Math.abs(delta) > Math.abs(maxDelta)) {
-                            maxDelta = delta;
-                        }
+            if (!beforeFingerCount && fingerCount) { // 开始触摸
+                console.log("开始触摸");
+                ++touchEventID; // 新建触摸事件
+                if (!slideEventID) { // 新建翻页事件
+                    slideEventID = touchEventID;
+                    // 触摸开始时间
+                    touchStartTime = new Date().getTime();
+                    // 停止当前的动画
+                    imageList.removeClass("slideTrans");
+                    imageList.unbind();
+                }
+            } else if (beforeFingerCount && !fingerCount) { // 结束触摸
+                console.log("结束触摸");
+                if (touchEventID != slideEventID) { // 在前一个翻页未完成前进行了触摸,将被忽略
+                    console.log("touchEventID=" + touchEventID + ", slideEventID=" + slideEventID);
+                    return;
+                }
+
+                // 判断动画条件
+                var prevX = curIndex * width * -1;
+                // 偏移量
+                var deltaX = Math.abs(curX - prevX);
+                // 触屏时间
+                var deltaTime = new Date().getTime() - touchStartTime;
+                // 是否满足翻页
+                var isSlide = false;
+                if (deltaX * 2>= width || deltaTime <= 200) { // 超过半屏 或者 触屏时间小于200毫秒
+                    isSlide = true;
+                }
+
+                // 满足翻页, 计算下一页的下标
+                if (deltaX != 0) {
+                    if (isSlide) {
+                        transImageList(curIndex + (curX < prevX ? 1 : -1));
+                    } else { // 否则回到原先位置
+                        transImageList(curIndex);
                     }
-                    touchEvent[fingerTouch.identifier] = fingerTouch.clientX;
+                } else { // 没有滑动, 不执行动画
+                    slideEventID = 0;
                 }
-            }
-            // 移动到指定偏移
-            var destX = curX + maxDelta;
-            if (destX > 0) { // 超出左边界
-                destX = 0;
-            } else if (destX < rightBound) { // 超出右边界
-                destX = rightBound;
-            }
-            setTranslateX(destX);
-        }).on("touchend touchcancel", function (event) {
-            // 防止事件向body冒泡
-            event.stopPropagation();
-
-            // 从touchEvent移除手指对应的记录
-            compareTouchFingers(event);
-
-            var touchEventRef = touchEvent;
-            // 所有手指都离开, 则当前触摸事件结束
-            if (!touchFinger) {
-                touchEvent = null;
-            }
-            // 在翻页未完成前触摸,将被忽略
-            if (touchEventRef != slideEvent) {
-                return;
-            }
-            // 只有所有手指都离开后, 才开始刷新动作
-            if (touchFinger) {
-                return;
-            }
-
-            // 判断动画条件
-            var prevX = curIndex * width * -1;
-            // 偏移量
-            var deltaX = Math.abs(curX - prevX);
-            // 触屏时间
-            var deltaTime = new Date().getTime() - touchStartTime;
-            // 是否满足翻页
-            var isSlide = false;
-            if (deltaX * 2>= width || deltaTime <= 200) { // 超过半屏 或者 触屏时间小于200毫秒
-                isSlide = true;
-            }
-
-            // 满足翻页, 计算下一页的下标
-            if (deltaX != 0) {
-                if (isSlide) {
-                    transImageList(curIndex + (curX < prevX ? 1 : -1));
-                } else { // 否则回到原先位置
-                    transImageList(curIndex);
+            } else if (beforeFingerCount) { // 正在触摸
+                console.log("正在触摸");
+                // 计算每个变化的手指, 取变化最大的delta
+                var maxDelta = 0;
+                for (var i = 0; i < event.originalEvent.changedTouches.length; ++i) {
+                    var fingerTouch = event.originalEvent.changedTouches[i];
+                    if (touchFingers[fingerTouch.identifier] !== undefined) {
+                        if (touchFingers[fingerTouch.identifier] !== null) {
+                            var delta = fingerTouch.clientX - touchFingers[fingerTouch.identifier];
+                            if (Math.abs(delta) > Math.abs(maxDelta)) {
+                                maxDelta = delta;
+                            }
+                        }
+                        touchFingers[fingerTouch.identifier] = fingerTouch.clientX;
+                    }
                 }
-            } else { // 没有滑动, 不执行动画
-                slideEvent = null;
+                if (touchEventID != slideEventID) {
+                    console.log("touchEventID=" + touchEventID + ", slideEventID=" + slideEventID);
+                    return;
+                }
+
+                // 移动到指定偏移
+                var destX = curX + maxDelta;
+                if (destX > 0) { // 超出左边界
+                    destX = 0;
+                } else if (destX < rightBound) { // 超出右边界
+                    destX = rightBound;
+                }
+                setTranslateX(destX);
             }
+
+
         });
     }
 };
